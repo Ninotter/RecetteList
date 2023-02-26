d783876f2d9792e419a95486f24c763496647e10
Ninotter <nino.didier@live.fr>
2023-02-26 11:15:20 +0100

    binding avance

--- app/src/main/java/com/nino/recettelist/model/RecipeListAdapter.kt
+++ app/src/main/java/com/nino/recettelist/model/RecipeListAdapter.kt
@@ -1,7 +1,40 @@
 package com.nino.recettelist.model
 
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.nino.recettelist.R
+import com.nino.recettelist.databinding.RecipeBinding
+import com.nino.recettelist.databinding.RecipeBindingImpl
 import com.nino.recettelist.dataclass.Recipe
+import com.nino.recettelist.viewmodels.RecetteListViewModel
 
-class RecipeListAdapter {
+class RecipeListAdapter(private val recipeList: List<Recipe>) : RecyclerView.Adapter<RecipeListAdapter.ViewHolder>()  {
 
+    private lateinit var binding: RecipeBinding
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        binding = RecipeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return recipeList.count()
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val recipe = recipeList[position]
+        holder.bind(recipe)
+    }
+
+
+
+    class ViewHolder(private val binding: RecipeBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(recipe: Recipe) {
+            binding.recipe = recipe
+        }
+    }
 }
--- app/src/main/java/com/nino/recettelist/ui/activities/RecetteListActivity.kt
+++ app/src/main/java/com/nino/recettelist/ui/activities/RecetteListActivity.kt
@@ -3,40 +3,66 @@ package com.nino.recettelist.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.LinearLayout
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.nino.recettelist.R
 import com.nino.recettelist.databinding.ActivityRecetteListBinding
 import com.nino.recettelist.dataclass.Recipe
+import com.nino.recettelist.model.RecipeListAdapter
 import com.nino.recettelist.viewmodels.RecetteListViewModel
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.android.awaitFrame
 import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import okhttp3.*
-import org.json.JSONArray
-import java.io.IOException
 
 
 class RecetteListActivity : AppCompatActivity() {
     lateinit var recetteListViewModel: RecetteListViewModel
-    lateinit var dataRecipe : List<Recipe>
+    var dataRecipe : List<Recipe> = listOf()
+    private lateinit var binding: ActivityRecetteListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_recette_list)
         recetteListViewModel = ViewModelProvider(this).get(RecetteListViewModel::class.java)
-        val btnRecherche: Button = findViewById(R.id.btnRecherche)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_recette_list)
+
+        val recyclerViewRecipe = binding.RecyclerViewRecettes
+        val recipeAdapter = RecipeListAdapter(dataRecipe)
+        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        recyclerViewRecipe.adapter = recipeAdapter
+        recyclerViewRecipe.layoutManager = layoutManager
+        recyclerViewRecipe.setHasFixedSize(true)
+        recyclerViewRecipe.addItemDecoration(
+            DividerItemDecoration(
+                this,
+                layoutManager.orientation
+            )
+        )
+
+        GlobalScope.async{
+            dataRecipe = recetteListViewModel.search();
+            InvokeRefreshBinding()
+        }
+        val btnRecherche: Button = findViewById(R.id.btnRecherche)
         btnRecherche.setOnClickListener {
             GlobalScope.async {
-                recetteListViewModel.search()
+                dataRecipe = recetteListViewModel.search()
+                InvokeRefreshBinding()
             }
         }
-        val binding: ActivityRecetteListBinding = DataBindingUtil.setContentView(this, R.layout.activity_recette_list);
-        binding.viewModel = recetteListViewModel
-        binding.lifecycleOwner = this;
-        GlobalScope.async{
-            recetteListViewModel.search();
-        }
+    }
+
+    public fun InvokeRefreshBinding(){
+        val recyclerViewRecipe = binding.RecyclerViewRecettes
+        val adapter = recyclerViewRecipe.adapter
+        adapter!!.notifyDataSetChanged()
     }
 }
